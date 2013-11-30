8ff7f7ed824b3178331a2bfcffeac797effb3d54
semprix <semprixd@gmail.com>
2013-12-01 07:01:35 +0800

    Added grabserver, grabcontent

--- modules/grabheader.py
+++ modules/grabheader.py
@@ -1,12 +1,21 @@
 import sys
 import urllib2
+import time
 
 def grabheader():
 	print "[*] Performing header analysis"
 	response = urllib2.urlopen('http://' + sys.argv[1])
-	grabheadresp = response.info().getheader('version')
-	if grabheadresp is None:
-		print "\033[91m>> Failed to get header value\033[0m"
+	grabserver = response.info().getheader('server')
+	print " [+] Grabbing web server version"
+	if grabserver is None:
+		print " \033[91m>> Failed to get header value\033[0m"
 	else:
-		print "Display"
+		print " \033[92m>> " + grabserver + "\033[0m"
+	grabcontent = response.info().getheader('Content-type')
+	print " [+] Grabbing content type version\033[0m"
+	if grabcontent is None:
+		print " \033[91m>> Failed to get header value\033[0m"
+	else:
+		print " \033[92m>> " + grabcontent + "\033[0m"
  	response.close()  
+ 	time.sleep(3)
