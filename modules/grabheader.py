a26618f5055ab89731312d970cf9e609032d4447
semprix <semprixd@gmail.com>
2013-12-01 07:04:29 +0800

    Line spacing

--- modules/grabheader.py
+++ modules/grabheader.py
@@ -8,13 +8,13 @@ def grabheader():
 	grabserver = response.info().getheader('server')
 	print " [+] Grabbing web server version"
 	if grabserver is None:
-		print " \033[91m>> Failed to get header value\033[0m"
+		print " \033[91m>> Failed to get header value *not vulnerable*\033[0m"
 	else:
 		print " \033[92m>> " + grabserver + "\033[0m"
 	grabcontent = response.info().getheader('Content-type')
 	print " [+] Grabbing content type version\033[0m"
 	if grabcontent is None:
-		print " \033[91m>> Failed to get header value\033[0m"
+		print " \033[91m>> Failed to get header value *not vulnerable*\033[0m"
 	else:
 		print " \033[92m>> " + grabcontent + "\033[0m"
  	response.close()  
