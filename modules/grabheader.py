f65ebcda89d26789d87949381d7c4b83f9b85424
semprix <semprixd@gmail.com>
2013-12-01 07:27:40 +0800

    Added non-standard response

--- modules/grabheader.py
+++ modules/grabheader.py
@@ -11,11 +11,34 @@ def grabheader():
 		print " \033[91m>> Failed to get header value *not vulnerable*\033[0m"
 	else:
 		print " \033[92m>> " + grabserver + "\033[0m"
-	grabcontent = response.info().getheader('Content-type')
-	print " [+] Grabbing content type version\033[0m"
+
+	grabplatform = response.info().getheader('x-powered-by')
+	print " [+] Grabbing platform \033[0m"
+	if grabplatform is None:
+		print " \033[91m>> No response from server\033[0m"
+	else:
+		print " \033[92m>> " + grabplatform + "\033[0m"
+
+	grabcontent = response.info().getheader('content-type')
+	print " [+] Grabbing content type \033[0m"
 	if grabcontent is None:
 		print " \033[91m>> Failed to get header value *not vulnerable*\033[0m"
 	else:
 		print " \033[92m>> " + grabcontent + "\033[0m"
+	grabframeopt = response.info().getheader('x-frame-options')
+	print " [+] Grabbing x-frame-options \033[0m"
+	if grabframeopt is None:
+		print " \033[91m>> No x-frame-options *vulnerability to click-jacking is possible*\033[0m"
+	else:
+		print " \033[92m>> " + grabframeopt + "\033[0m"
+	grabxssprotect = response.info().getheader('x-xss-protection')
+	print " [+] Grabbing x-xss-protection \033[0m"
+	if grabframeopt is None:
+		print " \033[91m>> No XSS protection *vulnerability to XSS is possible*\033[0m"
+	else:
+		print " \033[92m>> " + grabframeopt + "\033[0m"
+	
  	response.close()  
  	time.sleep(3)
+
+
