be07ca765408e3cfd352f84038ad7c69594df9f5
semprix <semprixd@gmail.com>
2013-12-01 07:53:43 +0800

    Updated with awesome banner

--- auxiliary/banner.py
+++ auxiliary/banner.py
@@ -4,24 +4,43 @@ import socket
 import os
 
 def exitbanner():
-	print('\033[94m******************************************\033[0m')
-	print('\033[94mFTW (Fortify The Web)\033[0m')
-	print('\033[94mFortify On Demand - HP Enterprise Security\033[0m')
-	print('\033[94mShadowLabs\033[0m')
-	print('\033[94m******************************************\033[0m')
-	print('\033[91mPlease provide target!\033[0m')
-  	print('\033[91mmUsage: python ftw.py target\033[0m')
-  	print('\033[91mExample: python ftw.py google.com 80\033[0m')
+
+	print" \033[94m ______         _   _  __    _______ _       __          __  _\033[0m"  
+	print" \033[94m|  ____|       | | (_)/ _|  |__   __| |      \ \        / / | |\033[0m"    
+	print" \033[94m| |__ ___  _ __| |_ _| |_ _   _| |  | |__   __\ \  /\  / /__| |__\033[0m"  
+	print" \033[94m|  __/ _ \| '__| __| |  _| | | | |  | '_ \ / _ \ \/  \/ / _ \ '_ \ \033[0m" 
+	print" \033[94m| | | (_) | |  | |_| | | | |_| | |  | | | |  __/\  /\  /  __/ |_) |\033[0m"
+	print" \033[94m|_|  \___/|_|   \__|_|_|  \__, |_|  |_| |_|\___| \/  \/ \___|_.__/ \033[0m"
+	print" \033[94m                           __/ |    ShadowLabs                     \033[0m"
+	print"  \033[94m                         |___/       \033[0m"
+	print""
+	print(' \033[94m******************************************\033[0m')
+	print(' \033[94mFTW (Fortify The Web)\033[0m')
+	print(' \033[94mFortify On Demand - HP Enterprise Security\033[0m')
+	print(' \033[94m******************************************\033[0m')
+	print(' \033[91mPlease provide target!\033[0m')
+  	print(' \033[91mmUsage: python ftw.py target\033[0m')
+  	print(' \033[91mExample: python ftw.py google.com 80\033[0m')
+  	print""
   	sys.exit()	
 	return;
 
 def startbanner():
-	print('\033[94m******************************************\033[0m')
-	print('\033[94mFTW (Fortify The Web)\033[0m')
-	print('\033[94mFortify On Demand - HP Enterprise Security\033[0m')
-	print('\033[94mShadowLabs\033[0m')
-	print('\033[94m******************************************\033[0m')
-	
+
+	print" \033[94m ______         _   _  __    _______ _       __          __  _\033[0m"  
+	print" \033[94m|  ____|       | | (_)/ _|  |__   __| |      \ \        / / | |\033[0m"    
+	print" \033[94m| |__ ___  _ __| |_ _| |_ _   _| |  | |__   __\ \  /\  / /__| |__\033[0m"  
+	print" \033[94m|  __/ _ \| '__| __| |  _| | | | |  | '_ \ / _ \ \/  \/ / _ \ '_ \ \033[0m" 
+	print" \033[94m| | | (_) | |  | |_| | | | |_| | |  | | | |  __/\  /\  /  __/ |_) |\033[0m"
+	print" \033[94m|_|  \___/|_|   \__|_|_|  \__, |_|  |_| |_|\___| \/  \/ \___|_.__/ \033[0m"
+	print" \033[94m                           __/ |    ShadowLabs                     \033[0m"
+	print"  \033[94m                         |___/       \033[0m"
+	print""
+	print(' \033[94m******************************************\033[0m')
+	print(' \033[94mFTW (Fortify The Web)\033[0m')
+	print(' \033[94mFortify On Demand - HP Enterprise Security\033[0m')
+	print(' \033[94m******************************************\033[0m')
+	print ""
 	print "Target:", sys.argv[1]
 	ip_add = socket.gethostbyaddr(sys.argv[1])
 	print "IP Address:", ip_add
