cd5675c9c30c6a895128cd3b25d04fa88789dd96
semprix <semprixd@gmail.com>
2013-11-30 08:22:19 +0800

    Unittesting

---
+++ unittest/hostalive.py
@@ -0,0 +1,11 @@
+import subprocess
+import shlex
+import sys
+
+command_line = "ping -c 1 " + sys.argv[1]
+
+try:
+      subprocess.check_call(command_line,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+      print "Website is there."
+except subprocess.CalledProcessError:
+      print "Couldn't get a ping."
---
+++ unittest/mysocket.py
@@ -0,0 +1,8 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect(('google.com', 22))
+    print "Port 22 reachable"
+except socket.error as e:
+    print "Error on connect: %s" % e
+s.close()
