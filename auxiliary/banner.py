35ab6826b4e02497a855919707a0b9d1bee75de4
semprix <semprixd@gmail.com>
2013-11-30 12:48:10 +0800

    Tester can select web ports between 80 and 443

--- auxiliary/banner.py
+++ auxiliary/banner.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 sys.path.append(r'auxiliary')
 from colorful import *
@@ -10,9 +11,9 @@ def exitbanner():
 	printout('ShadowLabs\n', BLUE)
 	printout('******************************************\n', BLUE)
 	printout('Please provide target!\n', RED)
-  	printout('Usage: python ftw.py path\n', WHITE)
-  	printout('Example: python ftw.py google.com\n\n', WHITE)
-  	sys.exit()
+  	printout('Usage: python ftw.py target\n', WHITE)
+  	printout('Example: python ftw.py google.com 80\n\n', WHITE)
+  	sys.exit()	
 	return;
 
 def startbanner():
@@ -23,8 +24,12 @@ def startbanner():
 	printout('******************************************\n', BLUE)
 	print "Target:", sys.argv[1]
 	print "Fortifying the Web now!!!"
-	print "\n"
-	print "[+++] Stage 1 Recon - Host Alive Check"
+
+def stage1():
+	print "[Stage 1] Recon"
+	print "[*] Checking if host is alive"
+	time.sleep(2)
+
 
 
 
--- ftw.py
+++ ftw.py
@@ -1,6 +1,8 @@
 # ftw.py - Fortify The Web
 # 
 #
+#
+#
 
 # Required Python libs
 import ping, socket
@@ -13,11 +15,10 @@ from termcolor import colored, cprint
 
 sys.path.append(r'auxiliary')
 from banner import *
-from colorful import has_colours, printout
 
 
 # Check if target is within the argument
-if len(sys.argv) < 2:
+if len(sys.argv) <= 2 :
   print
   exitbanner()
 
@@ -25,27 +26,36 @@ else:
 
 # Fortify the Web when ready
     startbanner()
-	
-# Check the default http port if we can connect	(80)
-print "[*] Checking for default http port (80)"
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Using socket to connect to default HTTP port
-try:
-    s.connect((sys.argv[1], 80))
-    cprint ('>> Target is up', 'green')
-except socket.error as e:
-    cprint ('>> Target is down', 'red')
-s.close()
+    stage1()
 
-# Check the default secure port if we can connect (443)
+# Check host if alive
 
-print "[*] Checking for default secure port (443)"
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Using socket to connect to secure (SSL) HTTP port
-try:
-    s.connect((sys.argv[1], 443))
-    cprint ('>> Target is up', 'green')
-except socket.error as e:
-    cprint ('>> Target is down', 'red')
-s.close()
+if sys.argv[2] == '80':
+    print "[*] Checking for default http port (80)"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Using socket to connect to default HTTP port
+    try:
+        s.connect((sys.argv[1], 80))
+        cprint ('>> Target is up', 'green')
+        cprint ('>> Marking for http testing', 'green')
+    except socket.error as e:
+        cprint ('>> Target is down', 'red')
+        s.close()
 
-time.sleep(3) # Give the app a little sleep
+elif sys.argv[2] == '443':
+    print "[*] Checking for default secure http port (443)"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Using socket to connect to default HTTP port
+    try:
+        s.connect((sys.argv[1], 443))
+        cprint ('>> Target is up', 'green')
+        cprint ('>> Marking for https testing', 'green')
+    except socket.error as e:
+        cprint ('>> Target is down', 'red')
+        s.close()
 
+else:
+     cprint ('[-] Not a default HTTP port', 'red')
+     cprint ('[*] Shutting down....', 'red')
+     time.sleep(3) # Give a little time to sleep
+
+
+ # Load modules for testing
