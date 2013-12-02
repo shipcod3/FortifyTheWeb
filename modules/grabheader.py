ec3ebe871767e667b2320139c9fe149714ce81cd
semprix <semprixd@gmail.com>
2013-12-02 11:45:06 +0800

    Added more checks

--- modules/grabheader.py
+++ modules/grabheader.py
@@ -6,42 +6,58 @@ import sys
 import urllib2
 import time
 
+from colors import *
+
 def grabheader():
 	print "[*] Performing header analysis"
 	response = urllib2.urlopen('http://' + sys.argv[1])
+
+	# Server header settings
+
 	grabserver = response.info().getheader('server')
 	print " [+] Grabbing web server version"
 	if grabserver is None:
-		print " \033[91m>> Failed to get header value *not vulnerable*\033[0m"
+		printout('Failed to get header value *not vulnerable*', RED)
 	else:
-		print " \033[92m>> " + grabserver + "\033[0m"
+		printout('  >> ' + grabserver, GREEN)
 
 	grabplatform = response.info().getheader('x-powered-by')
-	print " [+] Grabbing platform \033[0m"
+	print " [+] Grabbing platform "
 	if grabplatform is None:
-		print " \033[91m>> No response from server\033[0m"
+		printout('  >> No response from server', RED)
 	else:
-		print " \033[92m>> " + grabplatform + "\033[0m"
+		printout('  >> ' + grabplatform, GREEN)
 
 	grabcontent = response.info().getheader('content-type')
-	print " [+] Grabbing content type \033[0m"
+	print " [+] Grabbing content type "
 	if grabcontent is None:
-		print " \033[91m>> Failed to get header value *not vulnerable*\033[0m"
+		printout('  >> Failed to get header value *not vulnerable*', RED)
 	else:
-		print " \033[92m>> " + grabcontent + "\033[0m"
-	grabframeopt = response.info().getheader('x-frame-options')
-	print " [+] Grabbing x-frame-options \033[0m"
-	if grabframeopt is None:
-		print " \033[91m>> No x-frame-options *vulnerability to click-jacking is possible*\033[0m"
-	else:
-		print " \033[92m>> " + grabframeopt + "\033[0m"
-	grabxssprotect = response.info().getheader('x-xss-protection')
-	print " [+] Grabbing x-xss-protection \033[0m"
-	if grabframeopt is None:
-		print " \033[91m>> No XSS protection *vulnerability to XSS is possible*\033[0m"
-	else:
-		print " \033[92m>> " + grabframeopt + "\033[0m"
+		printout('  >> ' + grabcontent , GREEN)
+
+	 # Server header security settings
 	
+	 # Check if this is an SSL port
+
+	grabframeopt = response.info().getheader('x-frame-options')
+	print " [+] Grabbing x-frame-options "
+	if grabframeopt is None:
+		printout('  >>  No x-frame-options *vulnerability to click-jacking is possible*', RED)
+	else:
+		printout('  >> ' + grabframeopt, GREEN)
+	
+	grabxssprotect = response.info().getheader('x-xss-protection')
+	print " [+] Grabbing x-xss-protection "
+	if grabframeopt is None:
+		printout('  >> No XSS protection *vulnerability to XSS is possible*', RED)
+	else:
+		printout('  >> ' + grabframeopt, GREEN)
+	
+	grabxcontent = response.info().getheader('x-content-type-options')
+	print " [+] Grabbing x-content-type-options"
+	if grabxcontent is None:
+		printout('  >> No x-content-type-options *vulnerability to MIME sniffing is possible*', RED)
+ 	
  	response.close()  
  	time.sleep(3)
 
