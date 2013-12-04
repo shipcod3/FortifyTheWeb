3904746585b07cf3214ad067cbcc8e0a0d434e5f
semprix <semprixd@gmail.com>
2013-12-04 08:36:49 +0800

    Added SSL check enhancement for issue #12

--- modules/recon/urlcrawler.py
+++ modules/recon/urlcrawler.py
@@ -2,21 +2,35 @@
 # description: crawls the URL
 # author: @shipcod3
 
+import sys
 import requests
 import time
 
+sys.path.append(r'auxiliary')
 from bs4 import BeautifulSoup 
 from colors import *
 
 def urlcrawler():
-    print "[*] Crawling the target"
-    req  = requests.get("http://" + sys.argv[1])
-	
-    data = req.text
-    
-    soup = BeautifulSoup(data)
-    
-    for link in soup.find_all('a'):
-        print(link.get('href'))
-	 
+    print "[**] Starting Spider"
     time.sleep(3)
+    if sys.argv[2] == '80':
+    	print (' [+] Crawling HTTP')
+    	req  = requests.get("http://" + sys.argv[1])
+    	data = req.text
+    	soup = BeautifulSoup(data)
+    	for link in soup.find_all('a'):
+       		print(link.get('href'))
+
+    elif sys.argv[2] == '443':
+    	print (' [+] Crawling HTTPS')
+    	req  = requests.get("http://" + sys.argv[1])
+    	data = req.text
+    	soup = BeautifulSoup(data)
+    	for link in soup.find_all('a'):
+       		print(link.get('href'))
+
+    else:
+    	print ('error')
+
+	
+	time.sleep(3)
