72a7bdd3476964cc9d987cbc33e41858a77bcbb9
semprix <semprixd@gmail.com>
2014-01-08 09:36:44 +0800

    used sockets for portscanning

--- modules/recon/portScan.py
+++ modules/recon/portScan.py
@@ -7,16 +7,22 @@ import sys, os, socket
 from colors import *
 
 def portScan():
-	print '[**] Starting port scan......'
-	target = sys.argv[1]
-	targetIP = socket.gethostbyname(target)
-	
-	for i in range(443, 450):
-         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+	target    = sys.argv[1]
+	targetIP  = socket.gethostbyname(target)
 
-         result = s.connect_ex((targetIP, i))
-
-        if(result == 0) :
-            print 'Port %d: OPEN' % (i,)
-        s.close()
-
+try:
+    for port in range(1,1025):  
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((targetIP, port))
+        if result == 0:
+            print "Port {}: \t Open".format(port)
+        sock.close()
+ 
+except socket.gaierror:
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+ 
+except socket.error:
+    print "Couldn't connect to server"
+    sys.exit()
