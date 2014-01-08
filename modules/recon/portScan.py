0cf975a72f33d2c5c8cfcd01e27e970340e4c9e2
semprix <semprixd@gmail.com>
2014-01-08 08:52:15 +0800

    added portscan module

--- modules/recon/portScan.py
+++ modules/recon/portScan.py
@@ -1,5 +1,5 @@
 # portScan.py
-# description: fortify the web port scanner module
+# description: ftw port scanner module
 # author: @semprix
 
 import sys, os, socket
@@ -11,4 +11,12 @@ def portScan():
 	target = sys.argv[1]
 	targetIP = socket.gethostbyname(target)
 	
-	print targetIP
+	for i in range(443, 450):
+         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+         result = s.connect_ex((targetIP, i))
+
+        if(result == 0) :
+            print 'Port %d: OPEN' % (i,)
+        s.close()
+
