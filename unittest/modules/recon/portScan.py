009af933edd43b0febb837b6fece4286846ba941
semprix <semprixd@gmail.com>
2014-01-08 08:51:57 +0800

    added portscan unittest

--- unittest/modules/recon/portScan.py
+++ unittest/modules/recon/portScan.py
@@ -2,22 +2,19 @@
 # description: port scanner for ftw
 # author: @semprix
 
-import os, sys
-from socket import *
-
+import os, sys, socket
 
 print '[**] Starting port scan'
 target = sys.argv[1]
-targetIP = gethostbyname(target)
-
+targetIP = socket.gethostbyname(target)
+print targetIP
     #scan reserved ports
-    for i in range(20, 1025):
-        s = socket(AF_INET, SOCK_STREAM)
+for i in range(22, 1025):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        result = s.connect_ex((targetIP, i))
-
-        if(result == 0) :
-            print 'Port %d: OPEN' % (i,)
-        s.close()
+    result = s.connect_ex((targetIP, i))
+    if(result == 0) :
+	    print 'Port %d: OPEN' % (i,)
+    s.close()
 
 print targetIP
