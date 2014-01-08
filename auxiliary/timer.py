cca01a6ed979b7308509cdc459749aedec6e90d5
semprix <semprixd@gmail.com>
2014-01-08 09:36:20 +0800

    deprecated timer

--- auxiliary/timer.py
+++
@@ -1,9 +0,0 @@
-# timer.py
-# description: for time stamping
-# author: @semprix
-
-from time import localtime, strftime
-
-def timer():
-	endtime=strftime("%a, %d %b %Y %X +0000", localtime())
-	print endtime
