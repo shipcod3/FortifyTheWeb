62b9a63a8aa5da2a507e7be52749a4b621d9fd98
semprix <semprixd@gmail.com>
2014-02-27 21:55:21 +0800

    Fixed indent

--- modules/recon/portScan.py
+++ modules/recon/portScan.py
@@ -8,21 +8,21 @@ from colors import *
 
 def portScan():
  
-	target    = sys.argv[1]
-	targetIP  = socket.gethostbyname(target)
+     target    = sys.argv[1]
+     targetIP  = socket.gethostbyname(target)
 
-try:
-    for port in range(1,1025):  
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((targetIP, port))
+     try:
+        for port in range(1,1025):  
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((targetIP, port))
         if result == 0:
             print "Port {}: \t Open".format(port)
-        sock.close()
+            sock.close()
  
-except socket.gaierror:
-    print 'Hostname could not be resolved. Exiting'
-    sys.exit()
+     except socket.gaierror:
+        print 'Hostname could not be resolved. Exiting'
+        sys.exit()
  
-except socket.error:
-    print "Couldn't connect to server"
-    sys.exit()
+     except socket.error:
+        print "Couldn't connect to server"
+        sys.exit()
