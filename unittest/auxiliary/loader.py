4bb25e10016e7540f4e42217473372ac926dc0bb
semprix <semprixd@gmail.com>
2014-03-04 05:55:21 +0800

    Added loader unittest

---
+++ unittest/auxiliary/loader.py
@@ -0,0 +1,12 @@
+import time, sys
+from sys import stdout
+from termcolor import colored, cprint
+
+def loader(text):
+    for c in text:
+        print c,
+        sys.stdout.flush()
+        time.sleep(0.9)
+
+loader('*=*=*=*=*=*=*=*\n')
+print ">> Finishing"
