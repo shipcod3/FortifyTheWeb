6065f8bd58acd3a4c545e5c3f829f32909907043
semprix <semprixd@gmail.com>
2014-03-27 04:47:45 +0800

    removed portscan, moved dnsmiscon module

---
+++ modules/discovery/dnsMiscon.py
@@ -0,0 +1,24 @@
+# dnsMiscon.py
+# description: checks if the domain has a dns misconfiguration
+# author: @shipcod3
+# reference: http://www.securityfocus.com/archive/1/486606 
+
+import sys, socket, time
+
+from colors import *
+
+def dnsMiscon():
+    print "[**] Checking localhost."+sys.argv[1]+" for DNS Misconfiguration......"
+    
+    #resolve the host
+
+    print "[+]Resolving the host address"
+    try:
+        ip_addr = socket.gethostbyaddr("localhost."+sys.argv[1])
+        if ip_addr[0] == "localhost":
+            printout('  >> Vulnerable to DNS Misconfiguration which leads to Same-Site Scripting', RED)
+    except socket.error:
+        printout('  >> Not Vulnerable', GREEN)   
+    
+    print "[==] Done checking for DNS Misconfiguration"
+    time.sleep(3)
