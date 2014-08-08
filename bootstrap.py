51d5f6f2c67955060766c46d52419b6a522a0fdd
semprix <semprixd@gmail.com>
2014-08-09 07:37:12 +0800

    added config file check

--- bootstrap.py
+++ bootstrap.py
@@ -2,22 +2,30 @@
 # description: Dependency checker, bootstrapping FTW.
 # author: @semprix
 
-import sys, os, time
+import sys, os, time, ConfigParser
 
 ftwmods =['socket','bs4','requests','termcolor','ping', 'ConfigParser']
 
+# define your ftw directory installation
+config = ConfigParser.ConfigParser()
+config.read("config/config.ini")
+ftwdir=config.get("path", "ftwdir")
+
 print "[***] BootStrapping FortifyTheWeb. Please wait....."
-print "  [+] Getting OS version"
+time.sleep (5)
+print "[+] Getting OS version"
 ftwenv = os.uname()
 print        ftwenv
-
-print "  [+] Checking for required python modules"
+print "[+] Checking FTW data path"
+print "[**] Data path found on:" + ftwdir + " (define path if needed)"
+print ""
+print "[+] Checking for required python modules"
 for module_name in ftwmods:
   try:
     __import__(module_name)
     print " ++ Module %s found." %(module_name) 
   except ImportError:
     print "\033[91m -- Please install dependency ==>> (%s). \033[0m" %(module_name)
-print "Don't forget to define your configuration in config/config.ini"
+print ""
 print "BootStrap finishing"
 time.sleep(3)
