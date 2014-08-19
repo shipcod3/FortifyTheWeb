51af963cf080b042085d1d6b8d74239f195039f8
Jay Turla <shipcodez@gmail.com>
2014-08-19 15:44:52 +0800

    Update backup_finder.py

--- unittest/modules/recon/backup_finder.py
+++ unittest/modules/recon/backup_finder.py
@@ -7,7 +7,7 @@ import urllib2, re
 from urllib2 import URLError
 
 host = sys.argv[1]
-payload = open("payloads.txt")
+payload = open("../../../../payloads.txt")
 
 for backup_files in payload.readlines():
     
